c99179247438e9663dec72be66b737068f4aaacb
Emily Wozniak <ewozniak4623+git@gmail.com>
2023-10-02 18:09:29 -0400

    Added Dark Mode

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,6 @@
-// JavaScript goes here
+const checkbox = document.getElementById('checkbox');
+const themeStyle = document.getElementById('theme-style');
+
+checkbox.addEventListener('change', () => {
+    themeStyle.href = checkbox.checked ? 'styles/dark-theme.css' : 'styles/light-theme.css';
+});
