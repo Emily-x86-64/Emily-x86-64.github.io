8aa1d6ae176bd84b6f58e6a2f896702f9ace80ca
Emily Wozniak <ewozniak4623@gmail.com>
2023-10-23 17:47:14 -0400

    Changed Dark mode

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,8 @@
 const checkbox = document.getElementById('checkbox');
 const themeStyle = document.getElementById('theme-style');
+const themeSwitchText = document.getElementById('theme-switch-text');
 
-checkbox.addEventListener('change', () => {
+themeSwitchText.addEventListener('click', () => {
+    checkbox.checked = !checkbox.checked; // Toggle the checkbox
     themeStyle.href = checkbox.checked ? 'styles/dark-theme.css' : 'styles/light-theme.css';
 });
